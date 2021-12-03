50be80cba8a97ae8558a0a29d13ab9333794b36e
hoangnhut <hoangnhutsp@gmail.com>
2021-12-03 19:16:42 +0700

    UPDATE: HELLO

--- index.js
+++ index.js
@@ -111,19 +111,23 @@ io.on('connection', socket => {
 
 app.use(sessionMiddleware);
 
-app.use('/user', userRouters)
-app.use('/cart', cartRoutes)
-app.use('/evaluate', evaluateRouters)
-app.use('/product', productRoutes);
-app.use('/category', categoryRoutes);
-app.use('/recently_viewd', recentlyViewd)
-app.use('/upload_image', uploadImageRoutes);
-app.use('/address', addressRoutes);
-app.use('/invoice', invoiceRoutes);
-app.use('/admin', adminRouter);
-app.use('/notification', notificationRouter);
-app.use('/test', testRouter);
+// app.use('/user', userRouters)
+// app.use('/cart', cartRoutes)
+// app.use('/evaluate', evaluateRouters)
+// app.use('/product', productRoutes);
+// app.use('/category', categoryRoutes);
+// app.use('/recently_viewd', recentlyViewd)
+// app.use('/upload_image', uploadImageRoutes);
+// app.use('/address', addressRoutes);
+// app.use('/invoice', invoiceRoutes);
+// app.use('/admin', adminRouter);
+// app.use('/notification', notificationRouter);
+// app.use('/test', testRouter);
 
+
+app.get('/', (req, res) => {
+    res.send('HI')
+})
 
 mongoose.connect(CONNECTION_URL, dbOptions)
     .then(() => {
